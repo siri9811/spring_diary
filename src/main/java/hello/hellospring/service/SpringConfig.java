ef2c62b6a9ae177889afa9f45d0ec0a20747c039
Duchul <guscjfqkr9811@gmail.com>
2024-11-14 14:39:45 +0900

    스프링 JdbcTemplate

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -2,6 +2,7 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ public class SpringConfig {
     }
     //Dependeny Injection -> 필드 주입, setter 주입 ,*생성자 주입* 3가지 방법 존재
     //bean으로 등록되어있지 않으면 Autowired 작동 X
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -29,7 +31,8 @@ public class SpringConfig {
 // helloController -> MemberService -> MemberRepository
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
     }
 }
 
